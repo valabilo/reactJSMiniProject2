8553d79178a53dad28984adfc14ba6a0a516216b
valabilo <abilovalkrystoper@gmail.com>
2023-02-25 19:18:19 +0800

    final

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const nametest = 'Peter';
-
-  const handleNameChanges = () => {
-    const names = ['Bob', 'Juan', 'Peter'];
-    const int =  Math.floor(Math.random() * 3);
-    return names[int];
-  }
+  
   return (
     <div className="App">
       <Header />
       <Content />
+      <Footer />
+
     </div>
   );
 }
